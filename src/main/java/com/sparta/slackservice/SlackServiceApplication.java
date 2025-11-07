ff01d867e5b5b226f5e1d585f7ffd529deba9f01
SeongYeon Han <cicle1999@gmail.com>
2025-11-07 13:54:47 +0900

    add: 기본 SpringSecurity 설정 해제

--- src/main/java/com/sparta/slackservice/SlackServiceApplication.java
+++ src/main/java/com/sparta/slackservice/SlackServiceApplication.java
@@ -2,8 +2,9 @@ package com.sparta.slackservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SlackServiceApplication {
 
     public static void main(String[] args) {
