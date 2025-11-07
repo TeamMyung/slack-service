6c289aa3542907f6267ef1b32ebacf7fd8461681
SeongYeon Han <cicle1999@gmail.com>
2025-11-07 14:27:04 +0900

    fix: url 복수형으로 수정

--- src/main/java/com/sparta/slackservice/controller/SlackController.java
+++ src/main/java/com/sparta/slackservice/controller/SlackController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/slack")
+@RequestMapping("/v1/slacks")
 public class SlackController {
 
     private final SlackService slackService;
