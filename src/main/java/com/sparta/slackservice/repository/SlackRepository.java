663042686ced5ac5fcecfd685fc7e5e82b0db140
SeongYeon Han <cicle1999@gmail.com>
2025-11-07 14:45:26 +0900

    fix: repository id 형태 수정

--- src/main/java/com/sparta/slackservice/repository/SlackRepository.java
+++ src/main/java/com/sparta/slackservice/repository/SlackRepository.java
@@ -3,5 +3,7 @@ package com.sparta.slackservice.repository;
 import com.sparta.slackservice.domain.SlackMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SlackRepository extends JpaRepository<SlackMessage, Long> {
+import java.util.UUID;
+
+public interface SlackRepository extends JpaRepository<SlackMessage, UUID> {
 }
