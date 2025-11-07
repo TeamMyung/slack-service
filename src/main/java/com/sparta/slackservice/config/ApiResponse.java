01c3c902e63b64baca3d80d9537c00b7c8168385
SeongYeon Han <cicle1999@gmail.com>
2025-11-07 09:56:35 +0900

    add: config 추가

---
+++ src/main/java/com/sparta/slackservice/config/ApiResponse.java
@@ -0,0 +1,34 @@
+package com.sparta.slackservice.config;
+
+import com.sparta.slackservice.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+public class ApiResponse<T> {
+
+    private int status;
+    private String message;
+    private T data;
+    private ApiError error;
+
+    public ApiResponse(T data) {
+        this.status = 200;
+        this.message = "OK";
+        this.data = data;
+        this.error = null;
+    }
+
+    public ApiResponse(ErrorCode error) {
+        this.status = error.getStatus().value();
+        this.message = error.getStatus().name();
+        this.data = null;
+        this.error = new ApiError(error.getCode(), error.getDetails());
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class ApiError {
+        private int code;
+        private String details;
+    }
+}
